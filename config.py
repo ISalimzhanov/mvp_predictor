6e1a0e382a82f0679000e895ad2ceab4c4fa663d
ISalimzhanov <iskabaur@gmai..com>
2020-08-29 20:14:05 +0300

    configs

---
+++ config.py
@@ -0,0 +1,13 @@
+start_year = 1980
+end_year = 2019
+
+adv_stat_attrs = ['ws', 'vorp', 'ts_pct']
+not_to_compare = ['name', 'age', 'year', 'ws', 'mp_per_G', 'mvp_score']
+per_game_stat_attrs = ['g', 'gs', 'age', 'pts_per_g', 'trb_per_g', 'ast_per_g', 'stl_per_g',
+                       'blk_per_g',
+                       'tov_per_g', 'mp_per_g', 'fg_pct']
+
+modification_rules = {'ignore': ['mvp_score'], 'all': ['g', 'ws', 'age', 'mp_per_g'], 'fg_pct': ['pts_per_g'],
+                      'ts_pct': ['pts_per_g']}
+
+output_feature = 'mvp_score'
---
+++ main.py
@@ -0,0 +1,27 @@
+from config import end_year
+from ml_methods.linear_regression import mvp_predictor
+from data_preparation.unknown_mvp import next_mvp_prediction_data
+import matplotlib.pyplot as plt
+import pandas as pd
+
+if __name__ == '__main__':
+    reg = mvp_predictor()
+    try:
+        nba_mvp_2020 = pd.read_csv(f'data/{end_year + 1}.csv', index_col=0)
+    except FileNotFoundError:
+        nba_mvp_2020 = next_mvp_prediction_data()
+    predicted_mvp = reg.predict(nba_mvp_2020.drop(columns=['name', 'basket_ref_chance']))
+    predicted_mvp = pd.DataFrame(predicted_mvp, columns=['mvp_score'])
+    predicted_mvp['name'] = nba_mvp_2020['name']
+    predicted_mvp['mvp_score'] = predicted_mvp['mvp_score'].sub(predicted_mvp['mvp_score'].min())
+    predicted_mvp['mvp_score'] = predicted_mvp['mvp_score'].div(predicted_mvp['mvp_score'].sum())
+    predicted_mvp['mvp_score'] *= 1010
+    predicted_mvp.to_csv('prediction.csv')
+
+    plt.bar(nba_mvp_2020['name'], nba_mvp_2020['basket_ref_chance'])
+    plt.tight_layout()
+    plt.show()
+    plt.clf()
+    plt.bar(nba_mvp_2020['name'], predicted_mvp['mvp_score'])
+    plt.tight_layout()
+    plt.show()
