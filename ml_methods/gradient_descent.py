fixed gradient descent sklearn

ml_methods/gradient_descent.py
@@ -4,64 +4,65 @@ import random
 import copy
 import time
 
+from sklearn.linear_model import SGDRegressor
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
-
 from config import output_feature
 from data_preparation.data_preparation import prepare_data
 
 
-def sgd_calc(x: pd.DataFrame, y: pd.DataFrame, max_iter: int, alpha: float) -> np.array:
+def calc_gradient(x: pd.DataFrame, loss: np.array, alpha: float, type: str, batch_size=50) -> np.array:
+    if type != 'sgd' and type != 'bgd' and type != 'mbgd':
+        raise ValueError
     random.seed(time.time())
-    x_ = copy.deepcopy(x)
-    x_['ones'] = np.ones(x_.shape[0])
-    coefs = np.random.rand(1, x_.shape[1])
-    for iter in range(max_iter):
-        y_hat = pd.DataFrame(data=np.array(x_.dot(coefs.T)), columns=[output_feature])
-        i = random.randint(0, x_.shape[0] - 1)
-        for j in range(coefs.shape[1]):
-            coefs[0, j] -= alpha * (y_hat - y.values).iloc[i, 0] * pow(x_.iloc[i, j], j)
-    return coefs
-
-
-def bgd_calc(x: pd.DataFrame, y: pd.DataFrame, max_iter: int, alpha: float, batch_size=50) -> np.array:
-    random.seed(time.time())
-    x_ = copy.deepcopy(x)
-    x_['ones'] = np.ones(x_.shape[0])
-    coefs = np.random.rand(1, x_.shape[1])
-
-    batches = np.arange(x_.shape[0])
-    batches = np.array_split(batches, batch_size)
-    for iter in range(max_iter):
-        batch_i = random.randint(0, len(batches) - 1)
-        y_hat = pd.DataFrame(data=np.array(x_.dot(coefs.T)), columns=[output_feature])
-        for j in range(coefs.shape[1]):
-            batch = batches[batch_i]
-            for i in range(len(batch)):
-                coefs[0, j] -= 1. / batch_size * alpha * (y_hat - y.values).iloc[i, 0] * pow(x_.iloc[i, j], j)
-    return coefs
+    grad = np.zeros(shape=(1, x.shape[1]))
+    if type == 'sgd':
+        i = random.randint(0, x.shape[0] - 1)
+        for j in range(x.shape[1]):
+            grad[0, j] += alpha * loss.iloc[i, 0] * pow(x.iloc[i, j], j)
+    elif type == 'bgd':
+        grad = alpha * np.dot(loss.T, x) / x.shape[0]
+    else:
+        batch_ind = random.randint(0, int(x.shape[0] / batch_size))
+        for j in range(x.shape[1]):
+            for i in range(batch_ind * batch_size, min(x.shape[0], (batch_ind + 1) * batch_size)):
+                grad[0, j] += 1. / batch_size * alpha * loss.iloc[i, 0] * pow(x.iloc[i, j], j)
+    return grad
 
 
 class GdReggression:
-    def __init__(self, type: str, alpha: float, max_iter: int):
+    # ToDo fix
+    def __init__(self, type: str, alpha: float, max_iter: int, is_adaptive=False):
+        if type != 'sgd' and type != 'bgd' and type != 'mbgd':
+            raise AttributeError
         self.type = type
         self.alpha = alpha
         self.max_iter = max_iter
         self._x = None
         self._y = None
         self.coef_ = None
+        self.is_adaptive = is_adaptive
 
     def fit(self, x: pd.DataFrame, y: pd.DataFrame) -> None:
-        self._x = x
-        self._y = y
+        self._x = copy.deepcopy(x)
+        self._y = copy.deepcopy(y)
         self.coef_calc()
 
     def coef_calc(self) -> None:
-        types = {'sgd': lambda x, y: sgd_calc(x, y, self.max_iter, self.alpha),
-                 'bgd': lambda x, y: bgd_calc(x, y, self.max_iter, self.alpha)}
-        try:
-            self.coef_ = types[self.type](x=self._x, y=self._y)
-        except KeyError as e:
-            print('Incorrect type', e)
+        random.seed(time.time())
+        self._x.insert(0, '_', np.ones(self._x.shape[0]))
+        self.coef_ = np.random.rand(1, self._x.shape[1])
+        for _ in range(self.max_iter):
+            y_hat = self._x.dot(self.coef_.T)
+            loss = y_hat - self._y.values
+            gradient = calc_gradient(self._x, loss, self.alpha, self.type)
+            if self.is_adaptive:
+                nloss = self._x.dot((self.coef_ - gradient).T) - self._y.values
+                if np.square(nloss[0]).sum() < np.square(loss[0]).sum():
+                    self.coef_ -= gradient
+                else:
+                    self.alpha /= 2
+            else:
+                self.coef_ -= gradient
 
     def predict(self, x: pd.DataFrame):
         x_ = copy.deepcopy(x)
@@ -70,7 +71,7 @@ class GdReggression:
         return y_pred
 
 
-def mvp_predictor():
+def mvp_predictor(**kwargs):
     try:
         x_train = pd.read_csv('data/x_train.csv', index_col=0)
         x_test = pd.read_csv('data/x_test.csv', index_col=0)
@@ -78,10 +79,12 @@ def mvp_predictor():
         y_test = pd.read_csv('data/y_test.csv', index_col=0)
     except FileNotFoundError:
         x_train, x_test, y_train, y_test = prepare_data()
-    reg = GdReggression(type='bgd', max_iter=1000, alpha=0.001)
-    reg.fit(x_train, y_train)
-    print('Coefs:', reg.coef_[1:, ])
-    print('Intercept_:', reg.coef_[0, :])
+    # reg = GdReggression(type='bgd', max_iter=5000, alpha=1, is_adaptive=True)
+    reg = SGDRegressor(max_iter=50000, warm_start=True, learning_rate='adaptive')
+    reg.fit(x_train, np.ravel(y_train))
+    print('Coefs:', reg.coef_)
+    print('Intercept_:', reg.intercept_)
+    print('Score:', reg.score(x_train, np.ravel(y_train)))
     y_pred = reg.predict(x_test)
     print('R2:', r2_score(y_test, y_pred))
     print('MAE:', mean_absolute_error(y_test, y_pred))